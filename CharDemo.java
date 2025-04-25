48b897b7e297cda7304278f85a8a8757e3d03eb9
faheem <faheemhassantahir@gmial.com>
2025-04-25 22:47:41 +0500

    Char data type with example

---
+++ CharDemo.java
@@ -0,0 +1,19 @@
+package DataType;
+
+public class CharDemo {
+
+	public static void main(String[]args) {
+		
+		char a ='M';
+		char b= 'N';
+		
+		System.out.println("first value "+a);
+		System.out.println("Second value " +b);
+		
+		char c=65;
+		System.out.println("Binary Value character 65:"+c);
+		
+	}
+
+}
+
