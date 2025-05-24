e75781a4f5dad1033053f034e83f8bf06d464daf
faheem <faheemhassantahir@gmial.com>
2025-05-25 01:37:06 +0500

    Arthimatic operation

---
+++ ArthimaticOperator.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class ArthimaticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// arthimatic operator  + ,- ,*, /,%;
+		
+		int a=5, b=10;
+		System.out.println("Addition "+(a+b));
+		
+
+	}
+
+}
