dad95ed9c8b195f31e56e115fe2728ec6c33386d
faheem <faheemhassantahir@gmial.com>
2025-05-25 02:28:12 +0500

    All arthimatic Operators

--- ArthimaticOperator.java
+++ ArthimaticOperator.java
@@ -5,6 +5,10 @@ public class ArthimaticOperator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// arthimatic operator  + ,- ,*, /,%;
+		
+		//int a,b operand
+		// a+b expression
+		
 		// addition 
 		int a=5, b=10;
 		System.out.println("Addition "+(a+b));
@@ -12,7 +16,17 @@ public class ArthimaticOperator {
         //subtraction 
 		
 		int c=10, d=5;
-		System.out.println("Subtraction"+ (c-d));
+		System.out.println("Subtraction"+(c-d));
+		  
+		// multiplication*
+		 int e=5,f=10;
+		 System.out.println("Multiplication"+(e*f));
+		 
+		 //Divided /
+		 System.out.println("Division"+(e/f));
+		 
+		 // Modulus 
+		 System.out.println("Modulus"+(e%f));
 	}
 
 }
