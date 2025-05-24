98260a2ee3c3c351837cc4ac628dfb0e5abfeae7
faheem <faheemhassantahir@gmial.com>
2025-05-25 01:54:32 +0500

    Subtraction

--- ArthimaticOperator.java
+++ ArthimaticOperator.java
@@ -5,11 +5,14 @@ public class ArthimaticOperator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// arthimatic operator  + ,- ,*, /,%;
-		
+		// addition 
 		int a=5, b=10;
 		System.out.println("Addition "+(a+b));
 		
-
+        //subtraction 
+		
+		int c=10, d=5;
+		System.out.println("Subtraction"+ (c-d));
 	}
 
 }
