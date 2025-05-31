1450e9dc5a6a050c6ab8c739c9cd92865390ce09
faheem <faheemhassantahir@gmial.com>
2025-05-31 11:52:51 -0700

    My New code examples

---
+++ LogicalOperator.java
@@ -0,0 +1,30 @@
+package operators;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     // logical Operator And &&, Or||, not equal To !=,
+		
+		 Boolean bol1 = true;
+	        Boolean bol2 = false;
+
+	        // Print initial values
+	        System.out.println("bol1: " + bol1);
+	        System.out.println("bol2: " + bol2);
+
+	        // Logical AND
+	        System.out.println("bol1 && bol2: " + (bol1 && bol2));
+
+	        // Logical OR
+	        System.out.println("bol1 || bol2: " + (bol1 || bol2));
+
+	        // Logical NOT
+	        System.out.println("!bol1: " + (!bol1));
+	        System.out.println("!bol2: " + (!bol2));
+
+	        // Not Equal
+	        System.out.println("bol1 != bol2: " + (bol1 != bol2));
+
+}
+}
---
+++ RelationOperator.java
@@ -0,0 +1,37 @@
+package operators;
+
+public class RelationOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// output will be an boolean value true /false
+		
+//		// Greater Than 
+//		int a =10;
+//		int b=20;
+//		boolean bol=a>b;
+//		System.out.println(bol);
+//		
+//		// greater than and equal to
+//		int c=20;
+//		int d= 30;
+//		boolean bol2=c<=d;
+//		System.out.println(bol2);
+		
+		//Not equal to 
+		
+		int e =20;
+		int f =20;
+		boolean bol3= e!=f;
+		System.out.println(bol3);
+		
+		// == compare operator
+		int a=10;
+		int b=20;
+		boolean bol4=a==b;
+		System.out.println(bol4);
+		
+
+	}
+
+}
